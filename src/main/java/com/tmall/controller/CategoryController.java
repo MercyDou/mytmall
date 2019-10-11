d185c32121a74d5af386c838ef45a73b71b2a30f
MercyDou <2983055254@qq.com>
2019-10-11 14:02:05 +0800

    正在完善中..

--- src/main/java/com/tmall/controller/CategoryController.java
+++ src/main/java/com/tmall/controller/CategoryController.java
@@ -1,25 +1,25 @@
-package com.tmall.controller;
+    package com.tmall.controller;
 
-import com.tmall.entity.Category;
-import com.tmall.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+    import com.tmall.entity.Category;
+    import com.tmall.service.CategoryService;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.jws.WebParam;
-import java.util.List;
+    import javax.jws.WebParam;
+    import java.util.List;
 
-@Controller
-@RequestMapping("")
-public class CategoryController {
-    @Autowired
-    CategoryService categoryService;
+    @Controller
+    @RequestMapping("")
+    public class CategoryController {
+        @Autowired
+        CategoryService categoryService;
 
-    @RequestMapping("admin_category_list")
-    public String list(Model model) {
-        List<Category> cs = categoryService.listCategory();
-        model.addAttribute("cs", cs);
-        return "admin/listCategory";
+        @RequestMapping("admin_category_list")
+        public String list(Model model) {
+            List<Category> cs = categoryService.listCategory();
+            model.addAttribute("cs", cs);
+            return "admin/listCategory";
+        }
     }
-}
